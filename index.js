95d4f0cacedab67fbea7827dae285a7581b4eb72
Juan Barajas <juan.barajas4406@my.riohondo.edu>
2023-05-26 11:09:58 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then(function(respone){
+      return respone.json();
+    })
+    .then(function(data){
+      renderBooks(data);
+    })
 }
 
 function renderBooks(books) {
